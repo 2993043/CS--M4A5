b2aafb613a3dd7c3b9ab03b3cf28ae8eacc242bc
2993043 <2993043@stocktonusd.org>
2025-11-20 21:44:23 +0000

    This is the second coding assignment for CS :)

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+#1st part
+import math
+radius = (int(input("What is the radius?")))
 
+area = (str(math.pi * (radius**2)))
+print("Your area is: "+str(area))
+
+#2nd part
+a= (int(input("What is the a value?")))
+b= (int(input("What is the b value?")))
+
+answer = (str((a-b)+(a*b)))
+print("Your answer is: "+str(answer))
+
+#3rd part
+print("  /\_/\     /\_/\     /\_/\     /\_/\      ^___^ ")
+print(" (o . o)   (o . o)   (o . o)   (o . o)    /     \ ")
+print(" >  ^  <   >  ^  <   >  ^  <   >  ^  <   ( *   * ) ")
+print("    /\_/\     /\_/\     /\_/\     /\_/\  {>     <} ")
+print("   (o . o)   (o . o)   (o . o)   (o . o)  \     / ")
+print(" /\_/\    /\_/\   /\_/\    /\_/\     /\_/\       /\_/\                            ")
+print(" /\_/\     /\_/\     /\_/\     /\_/\     /\_/\     /\_/\ ")
+print("(o . o)   (o . o)   (o . o)   (o . o)   (o . o)   (o . o) ")
+print(">  ^  <   >  ^  <   >  ^  <   >  ^  <   >  ^  <   >  ^  < ")
